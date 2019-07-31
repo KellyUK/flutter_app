a3584250b5f556106f8a28070dd95ce25190bab1
Kelly Davidson <kellyadavidsonuk@gmail.com>
2019-07-31 14:49:36 +0100

    added Home.dart and extracted out homepage widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
+import './screens/home.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-    title: "hello Flutter App",
-    home:  Scaffold(appBar: AppBar(
-      title: Text("Title in App Bar"),
-    ),
-      body: Material(
-      color: Colors.deepPurple,
-      child: Center(
-        child: Text(
-          "Hello Flutter",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 36.0),
-    ),
-    ),
-  ),
-  ),
-    ));
+void main() => runApp(HelloFlutterApp());
+
+class HelloFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "hello Flutter App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Title in App Bar"),
+        ),
+        body: Home()
+      ),
+    );
+  }
 }
-
